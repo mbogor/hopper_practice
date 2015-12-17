3e679d227fb1d12502ddf63d890235559086c4ce
Mariya B <mb2283@cornell.edu>
2015-12-16 23:52:54 -0500

    Create loops.js
    04_loops

---
+++ loops.js
@@ -0,0 +1,105 @@
+var repeat = function(a,b) {
+	var result = "";
+	if (b==0){
+		return result;
+	}
+
+	while (b>0){
+		result+=a;
+		b--;
+	}
+	return result;
+}
+
+
+var sum = function(array){
+	var total = 0;
+	if (array.length==0){
+		return 0;
+	} 
+	else if (array.length==1){
+		return array[0];
+	}
+	else {
+		for (var i=0; i<array.length; i++){
+			total+=array[i];
+		}
+		return total;
+	}
+}
+
+
+var gridGenerator = function(n){
+	var st = "";
+
+	for (var i=0; i<n; i++){
+
+		for (var j=0; j<n; j++){
+
+			if (i%2===0 && j%2===0 || i%2===1 && j%2===1)
+			{
+				st=st.concat("#");
+			}
+			else{
+				st=st.concat(" ");
+			}
+		}
+		st=st.concat("\n");
+	}
+	return st;
+}
+
+
+var largestTriplet = function (n) {
+	var answer = [];
+	for (i=n;i>1;i--) {
+		for (j=(i-1);j>1;j--) {
+			var low = Math.pow((Math.pow(i,2)) - (Math.pow(j,2)),.5);
+			if ((low%1===0) && (low>1)) {
+				answer.push(low);
+				answer.push(j);
+				answer.push(i);
+				return answer;
+			}
+		}
+	} 
+}
+
+
+var join = function(array, string){
+	var str = "";
+	
+	if (array.length===0){
+		return str;
+	
+	} else if (array.length!==0 && string===undefined) {
+		for (var i=0; i<array.length; i++){
+			str = str.concat(array[i]);
+		}
+		return str;
+
+	}else if (array.length!==0 && string!==undefined) {
+		for (var j=0;j<array.length; j++){
+			str = str.concat(array[j]+ string);
+			if (j==(array.length)-1){
+			    str = str.slice(0,(str.length-1));
+			}
+		}
+		return str;
+		
+	}
+}
+
+
+var paramify = function(obj){
+	var answer = "";
+	for (prop in obj){
+		answer = answer+ prop + "=" + obj[prop];
+		
+		if (Object.keys(obj).length!=(prop+1)){
+			answer = answer + "&";
+		}
+	}
+	return answer;
+
+}
